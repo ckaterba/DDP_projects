dd1e08af2c5e6f8c42c05c6a15558c7cb58b5491
ckaterba-fvcc <ckaterba@fvcc.edu>
2024-01-04 16:43:24 -0700

    update map layer for heatmap

---
+++ p1/secret.R
@@ -0,0 +1,8 @@
+#github personal access token, no quotes of course
+
+PAT <- "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"
+
+#strava stuff
+app_name <- "Stravenous"
+app_client_id <- 75951
+app_secret <- "33a13550e6dd59b51be1f2f4c18a5b0f4e382be7"
